39f5741aeb8d7a42540bdb2f6df92336c3011978
epifaniagb <epifaniaegb@gmail.com>
2023-10-04 14:47:54 -0400

    function 4

--- index.js
+++ index.js
@@ -21,4 +21,12 @@ const multiplesOfThreeOrFive = () => {
     }
   }
 };
-console.log(multiplesOfThreeOrFive());
+//console.log(multiplesOfThreeOrFive());
+
+const untilNum = (num) => {
+  for (let i = 1; i <= num; i++) {
+    console.log(i);
+  }
+};
+
+console.log(untilNum(10));
