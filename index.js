e449c1472bdf8b4aaaacf698c9132005b5850940
Aaron C <aaronc@MacBook-Pro.local>
2023-10-04 14:59:11 -0400

    function 7

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //Code your solutions in this file
+//1
 const fiveToOneHundred = () => {
   for (let i = 5; i <= 100; i++) {
     console.log(i);
@@ -6,6 +7,7 @@ const fiveToOneHundred = () => {
 };
 //console.log(fiveToOneHundred());
 
+//2
 const multiplesOfThree = () => {
   for (let i = 3; i <= 100; i += 3) {
     console.log(i);
@@ -14,6 +16,7 @@ const multiplesOfThree = () => {
 
 //console.log(multiplesOfThree());
 
+//3
 const multiplesOfThreeOrFive = () => {
   for (let i = 3; i <= 100; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
@@ -23,6 +26,7 @@ const multiplesOfThreeOrFive = () => {
 };
 //console.log(multiplesOfThreeOrFive());
 
+//4
 const untilNum = (num) => {
   for (let i = 1; i <= num; i++) {
     console.log(i);
@@ -31,11 +35,13 @@ const untilNum = (num) => {
 
 //console.log(untilNum(10));
 
+//5
 const multiply = (num1, num2) => {
   console.log(num1 * num2);
 };
 // console.log(multiply(3, 7.5));
 
+//6
 const add = (num1, num2) => {
   if (num1 != num2) {
     return num1 + num2;
@@ -44,4 +50,14 @@ const add = (num1, num2) => {
   }
 };
 
-console.log(add(2, 6));
+//console.log(add(2, 6));
+
+//7
+const isNegative = (num) => {
+  if (Math.sign(num) === -1 || Math.sign(num) === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(isNegative(-3));
